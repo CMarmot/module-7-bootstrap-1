e86ca388f831e9e004dfa2feb39abb9a2287dd84
Corlene M. Ankrum <C.ANKRUM@labs.clark.edu>
2019-02-19 12:52:55 -0800

    changes from class

--- page.php
+++ page.php
@@ -11,16 +11,23 @@
 </head>
 <body>
 <div class="container">
-    <section>
-        <h1>Evergreen</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
+    <div class="row">
+        <div class="col-lg-12 col-sm-6 col-md-8">
+    <section class="bg-info text-white p-5">
+        <img src="img/200px-Clark_College_logo.png"class="d-block mx-auto" alt="">
+        <div class="alert alert-success text-center font-weight-bold mt-4 p5">
+            Attention: It's snowing!
+</div>
+        <div class ="display-4">Evergreen</div>
+        <p class="lead font-weight-bold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
         <h2>Camas</h2>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
-        <h3>Vanmcouver</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
+        <h3 class="mt-4 p-5 font-italic">Vancouver</h3>
+        <p class="text-justify text-uppercase mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
         <h4>Comsewogue</h4>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
     </section>
+</div>
 </div>
 
 <!-- jQuery -->
